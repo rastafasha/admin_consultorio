0f89698649ea7ac8acf046c7139492c6820c2fca
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-02-28 18:51:33 -0400

    environmet local

---
+++ src/environments/environment.consultorio.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+   
+    url_backend: 'https://consultorio.health-connect.me/backend-api-citas/',
+    url_servicios: 'https://consultorio.health-connect.me/backend-api-consultorio/public/api',
+    url_frontend: 'https://consultorio.health-connect.me/',
+    url_media: 'https://consultorio.health-connect.me/backend-api-consultorio/storage/app/public/',
+
+    
+    
+    
+};
