53e8df15f84d985a4e4507cc8be73b4783831a42
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-03-22 21:11:43 -0400

    fix logo size login, env y nombre

--- src/environments/environment.consultorio.ts
+++ src/environments/environment.consultorio.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production:true,
    
-    url_backend: 'https://consultorio.health-connect.me/backend-api-citas/',
-    url_servicios: 'https://consultorio.health-connect.me/backend-api-consultorio/public/api',
-    url_frontend: 'https://consultorio.health-connect.me/',
-    url_media: 'https://consultorio.health-connect.me/backend-api-consultorio/storage/app/public/',
+    url_backend: 'https://consultorio.klyntic.com/backend-api-consultorio/',
+    url_servicios: 'https://consultorio.klyntic.com/backend-api-consultorio/public/api',
+    url_frontend: 'https://consultorio.klyntic.com/',
+    url_media: 'https://consultorio.klyntic.com/backend-api-consultorio/storage/app/public/',
 
     
     
