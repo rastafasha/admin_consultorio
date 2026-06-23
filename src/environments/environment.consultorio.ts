81a21ea8212eb6c1fbd477a6b5a7ea9b3d6ce1af
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-06-23 19:26:58 -0400

    despliegue en vercel

--- src/environments/environment.consultorio.ts
+++ src/environments/environment.consultorio.ts
@@ -1,16 +1,17 @@
 export const environment = {
-    production:true,
-   
-    url_backend: 'https://consultorio.klyntic.com/backend-api-consultorio/',
-    url_servicios: 'https://consultorio.klyntic.com/backend-api-consultorio/public/api',
-    url_frontend: 'https://consultorio.klyntic.com/',
-    url_media: 'https://consultorio.klyntic.com/backend-api-consultorio/storage/app/public/',
+    production: true,
+
+    url_backend: 'https://backend-api-consultorio-production.up.railway.app/',
+    url_servicios: 'https://railway.app',
+    url_media: 'https://backend-api-consultorio-production.up.railway.app/storage/',
     //conexion a node y manejo de notificaciones
-    backend_node:"https://back-klyntic-envios.onrender.com/api",
-    urlBackedNotification:'https://back-klyntic-envios.onrender.com/api/notipush/save-subscription',
+    backend_node: "https://back-klyntic-envios.onrender.com/api",
+
+    url_frontend: 'https://consultorio.klyntic.com/',
+    urlBackedNotification: 'https://back-klyntic-envios.onrender.com/api/notipush/save-subscription',
     VAPI_KEY_PUBLIC: 'BG-UDqYJkOikTb0G7nNdKcpqZm__XCl0dwbJsx-kerpEecxL5rp079U7UMZxqo5XA0i60NGOVlezm1RAMyHRTbQ',
-  
-    
-    
-    
+
+
+
+
 };
